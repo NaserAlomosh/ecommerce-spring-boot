129d0b970e21a6a699be75e5e96b4b2d52f484ff
Naser Alomosh <132396593+NaserAlomosh@users.noreply.github.com>
2026-07-20 20:56:36 +0300

    Add product image upload support

---
+++ src/main/java/com/smart/ecommerce/repository/InventoryMovementRepository.java
@@ -0,0 +1,6 @@
+package com.smart.ecommerce.repository;
+
+import com.smart.ecommerce.entity.InventoryMovement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryMovementRepository extends JpaRepository<InventoryMovement, Long> {}
---
+++ src/main/java/com/smart/ecommerce/storage/StoredFile.java
@@ -0,0 +1,3 @@
+package com.smart.ecommerce.storage;
+
+public record StoredFile(String imageUrl, String storagePath, String contentType, long fileSize) {}
